4fac12d2354f74be09c71d244d6efc9f52a1b5c5
OceanMac <96398524+OceanMac@users.noreply.github.com>
2024-03-19 14:22:09 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,19 +9,12 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script (After Update feeds)
 
-
-# luci-app-poweroff
-echo 'src-git esir https://github.com/esirplayground/luci-app-poweroff.git;master' >> feeds.conf.default
-
 # luci-app-passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
 # helloworld
 echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
-
-# luci-app-netdata
-echo "src-git netdata https://github.com/Jason6111/luci-app-netdata.git;main" >> feeds.conf.default
 
 # lienol package
 echo "src-git lienolpackage https://github.com/Lienol/openwrt-package.git;main" >> feeds.conf.default
