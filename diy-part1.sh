5a2fb877c7a04153028219dffecce45fd7582c81
OceanMac <96398524+OceanMac@users.noreply.github.com>
2025-02-14 19:32:25 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,11 @@
 # Description: OpenWrt DIY script (After Update feeds)
 
 # passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
 # helloworld
-echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
+# echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 
 # kenzok8 package
 echo "src-git smpackage https://github.com/kenzok8/openwrt-packages.git;master" >> feeds.conf.default
