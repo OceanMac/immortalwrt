d01d062f08af81c586bf27732971bff412b6b416
OceanMac <96398524+OceanMac@users.noreply.github.com>
2025-02-15 09:28:20 +0800

    Update

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,12 +10,5 @@
 # Description: OpenWrt DIY script (After Update feeds)
 
 
-# Add luci-app-poweroff
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff ./package/luci-app-poweroff
-
-# Add luci-app-netdata
-rm -rf ./feeds/luci/applications/luci-app-netdata/
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata ./feeds/luci/applications/luci-app-netdata/
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
