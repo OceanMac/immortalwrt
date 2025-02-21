53631d536ddc8364c0c0c281077fc8a90f619c4f
OceanMac <96398524+OceanMac@users.noreply.github.com>
2025-02-21 10:29:38 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,3 @@
 # passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-
-# packages
-echo "src-git immpackages https://github.com/immortalwrt/packages.git;master" >> feeds.conf.default
-
-# luci
-echo "src-git immluci https://github.com/openwrt/luci.git;master" >> feeds.conf.default
