8d22cd6ae7453e98dda4e901e571c09d25172dcd
OceanMac <96398524+OceanMac@users.noreply.github.com>
2025-02-22 20:12:50 +0800

    Create

---
+++ diy-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script (After Update feeds)
+
+# passwall
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+
+# packages
+echo "src-git immpackage https://github.com/immortalwrt/packages.git;master" >> feeds.conf.default
+
+# luci
+echo "src-git immluci https://github.com/openwrt/luci.git;master" >> feeds.conf.default
