8ac938f48bb76b4ffc5cf83474e559034af2cb43
OceanMac <96398524+OceanMac@users.noreply.github.com>
2025-02-24 13:58:09 +0800

    Update

--- configuration.sh
+++ configuration.sh
@@ -11,4 +11,8 @@
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Set default Theme
+default_theme='bootstrap'
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
